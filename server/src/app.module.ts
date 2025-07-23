5034e099fbfa1a558ee6f6a7bba0c0602d19d795
jeffrey_sanford@hotmail.com <jeffrey_sanford@hotmail.com>
2025-07-23 12:03:50 -0500

    Addressing the cve-2025-53770 vulnerabilities

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController, ApiController } from './app.controller';
 import { ReportsModule } from './reports/reports.module';
 import { ListsModule } from './lists/lists.module';
 
 @Module({
-  imports: [ReportsModule, ListsModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 900, // 15 minutes
+        limit: 100, // max requests per IP
+      },
+    ]),
+    ReportsModule,
+    ListsModule,
+  ],
   controllers: [AppController, ApiController],
 })
 export class AppModule {}
